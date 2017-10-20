550f1e666cf18a7dae3280e5b28e23ca860b1594
timon <nobelentiimon@gmail.com>
2017-10-20 16:17:39 +0200

    Network

--- client/Assets/Scripts/Network.cs
+++ client/Assets/Scripts/Network.cs
@@ -1,17 +1,43 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Colyseus;
 
-public class Network : MonoBehaviour {
+using GameDevWare.Serialization;
+using GameDevWare.Serialization.MessagePack;
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("lala");
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class Network : MonoBehaviour
+{
+
+    Client client;
+    Room hub;
+    public string serverName = "mem-server.herokuapp.com";
+    public string serverPort = "4200";
+    public string hubName = "hub";
+
+
+    // Use this for initialization
+    IEnumerable Start()
+    {
+        String uri = "ws://" + serverName + ":" + serverPort;
+        client = new Client(uri);
+        client.OnOpen += OnOpenHandler;
+        client.OnClose += (object sender, EventArgs e) => Debug.Log("CONNECTION CLOSED");
+
+
+        yield return StartCoroutine(client.Connect());
+
+        hub = client.Join(hubName);
+
+        OnApplicationQuit();
+    }
+    void OnOpenHandler (object sender, EventArgs e)
+    {
+        Debug.Log("Connectet to Server. Client id: " + client.id);
+    }
+    void OnApplicationQuit()
+    {
+        client.Close();
+    }
 }
