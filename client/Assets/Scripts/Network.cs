3942eb0819c2295a3f4b15dd9b3b53cfa95acd9b
timon <nobelentiimon@gmail.com>
2017-10-20 15:30:18 +0200

    update

---
+++ client/Assets/Scripts/Network.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Network : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("lala");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
